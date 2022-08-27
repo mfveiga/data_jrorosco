80c3e65c22a7690459fe6972e17f3806534234bb
mfveiga <53996187+mfveiga@users.noreply.github.com>
2022-08-27 17:23:06 -0300

    Update streamlit-jrorosco.py
    include list of selection into map

--- streamlit-jrorosco.py
+++ streamlit-jrorosco.py
@@ -10,11 +10,25 @@ import numpy as np
 from folium.features import DivIcon
 from folium import Popup
 
+import warnings
+warnings.filterwarnings('ignore')
+
 df = pd.read_excel('JROROSCO_VOTOS.xlsx')
 
 df['PERC_%']=(round(df[['PERC']]*100,1)).astype(str)+"%"
-df2 = df[['NM_LOCALIDADE','PERC_%']].reset_index(drop=True)
-st.dataframe(df2)
+# df2 = df[['NM_LOCALIDADE','PERC_%']].reset_index(drop=True)
+# st.dataframe(df2)
+st.dataframe(df)
+
+df_filter = df.select_dtypes(include=np.number).columns.to_list()
+df_filter.remove('LAT')
+df_filter.remove('LONG')
+
+
+select_item = st.radio(
+    "Select one",
+    df_filter
+)
 
 geo=r"geojs-35-mun.json"
 file = open(geo, encoding="utf8")
@@ -31,11 +45,17 @@ folium.Choropleth(
     fill_color="PuBu"
     ).add_to(m3)
 
-for loc, p, n in zip(zip(df["LAT"],df["LONG"]),df["PERC"],df["NM_LOCALIDADE"]):
+for loc, p, n in zip(zip(df['LAT'],df['LONG']),df[select_item],df['NM_LOCALIDADE']):
+
+    if select_item == 'PERC':
+        html='<div style="font-size: 10pt; color : black">'+"{:.1%}".format(p)+'</div>'
+    else:
+        html='<div style="font-size: 10pt; color : black">'+"{:.0f}".format(p)+'</div>'
+
     folium.Marker(loc, icon=DivIcon(
         icon_size=(0,0),
         icon_anchor=(10,0),
-        html='<div style="font-size: 10pt; color : black">'+"{:.1%}".format(p)+'</div>',)).add_to(m3)
+        html=html,)).add_to(m3)
     folium.Marker(loc,
         popup=Popup(n)).add_to(m3)
 
